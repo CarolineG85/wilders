8ad8afd3a32221332e7893bfecaa29c9e879b37c
CarolineG85 <caroline.gallix85@gmail.com>
2023-10-03 10:32:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import chalk from "chalk"
+
+console.log(
+    chalk.blue("Sissis")
+)
+
+console.log(
+    chalk.red("Akane")
+)
+
+console.log(
+    chalk.green("Nadine")
+)
+
+console.log(
+    chalk.yellow("Guillaume")
+)
